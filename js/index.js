1190b6ead8dd091aa1d0766dc7fb3592cf7b8670
Rahul <dasr16583@gmail.com>
2022-07-18 22:02:05 +0530

    changes in image and second about section are created

--- js/index.js
+++ js/index.js
@@ -1,7 +1,61 @@
+// for timmer
+
+// console.log("welcome");
+
+// let dayBox = document.getElementById("day-box");
+// let hrBox = document.getElementById("hr-box");
+// let minBox = document.getElementById("min-box");
+// let secBox = document.getElementById("sec-box");
+// let endDate = new Date(2022, 9, 30, 6, 30 );
+// let endTime = endDate.getTime();
+
+// function countdown(){
+//     let todayDate = new Date();
+//     let todayTime = todayDate.getTime();
+
+//     let reminingTime = endTime - todayTime;
+
+//     // 60 second = 1000 millisecond
+//     let oneMin = 60 * 1000;
+//     let oneHr = 60 * oneMin;
+//     let oneDay = 24 * oneHr;
+
+//     // function for formate number if it is single digit
+//     let addZeroes = num => num < 10 ? `0${num}`: num;
+
+//     // if end datee is before today date
+//     if(endTime < todayTime){
+//         clearInterval(i)
+//         document.querySelector('.countdown').innerHTML = `<h1>Countdown had expired</h1>`
+//     }else{
+//         let daysLeft = Math.floor(reminingTime / oneDay);
+
+//         let hrsLeft = Math.floor((reminingTime % oneDay) / oneHr);
+
+//         let minsLeft = Math.floor((reminingTime % oneHr) / oneMin);
+
+//         let secsLeft = Math.floor((reminingTime % oneMin) / 1000);
+        
+//         dayBox.textContent = addZeroes(daysLeft);
+//         dayBox.textContent = addZeroes(hrsLeft);
+//         dayBox.textContent = addZeroes(minsLeft);
+//         dayBox.textContent = addZeroes(secsLeft);
+//     }
+// }
+
+// let  i = setInterval(countdown,1000);
+// countdown();
+
+
+
+// for scroll to top
+
 const scrollElement = document.querySelector(".scroll-top");
 
-const scrollTop = () => {
-    firstsection.scrollIntoView({ behaviour: "smooth" });
+const scrollMax = document.querySelector(".firstsection");
+
+const scrollTop = ()=>{
+    scrollMax.scrollIntoView({ behaviour: "smooth" });
 };
 
 scrollElement.addEventListener("click", scrollTop);
