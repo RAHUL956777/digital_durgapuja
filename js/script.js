02718268f53df58dc0dd52f5ad73de23c0827698
Rahul <dasr16583@gmail.com>
2023-04-18 20:59:37 +0530

    done few changes

--- js/script.js
+++ js/script.js
@@ -5,48 +5,47 @@ let minBox = document.getElementById("min-box");
 let secBox = document.getElementById("sec-box");
 
 // year, day, hour, second
-let endDate = new Date(2023, 9, 6, 12,);
+let endDate = new Date(2023, 9, 6, 12);
 let endTime = endDate.getTime();
 
+function countdown() {
+  let todayDate = new Date();
+  let todayTime = todayDate.getTime();
 
-function countdown(){
-    let todayDate = new Date();
-    let todayTime = todayDate.getTime();
+  let reminingTime = endTime - todayTime;
 
-    let reminingTime = endTime - todayTime;
+  // 60 second = 1000 millisecond
+  let oneMin = 60 * 1000;
+  let oneHr = 60 * oneMin;
+  let oneDay = 24 * oneHr;
 
-    // 60 second = 1000 millisecond
-    let oneMin = 60 * 1000;
-    let oneHr = 60 * oneMin;
-    let oneDay = 24 * oneHr;
+  // function for formate number if it is single digit
+  let addZeroes = (num) => (num < 10 ? `0${num}` : num);
 
-    // function for formate number if it is single digit
-    let addZeroes = num => num < 10 ? `0${num}`: num;
+  // if end datee is before today date
+  if (endTime < todayTime) {
+    clearInterval(i);
+    document.querySelector(
+      ".countdown"
+    ).innerHTML = `<h1>Countdown had expired!</h1>`;
+  } else {
+    let daysLeft = Math.floor(reminingTime / oneDay);
 
-    // if end datee is before today date
-    if(endTime < todayTime){
-        clearInterval(i)
-        document.querySelector(".countdown").innerHTML = `<h1>Countdown had expired!</h1>`;
-    }else{
-        let daysLeft = Math.floor(reminingTime / oneDay);
-        
-        let hrsLeft = Math.floor((reminingTime % oneDay) / oneHr);
+    let hrsLeft = Math.floor((reminingTime % oneDay) / oneHr);
 
-        let minsLeft = Math.floor((reminingTime % oneHr) / oneMin);
-        
-        let secsLeft = Math.floor((reminingTime % oneMin) / 1000);
-        
-        // displaying values
-        dayBox.textContent = addZeroes(daysLeft);
-        hrBox.textContent = addZeroes(hrsLeft);
-        minBox.textContent = addZeroes(minsLeft);
-        secBox.textContent = addZeroes(secsLeft);
-    }
+    let minsLeft = Math.floor((reminingTime % oneHr) / oneMin);
+
+    let secsLeft = Math.floor((reminingTime % oneMin) / 1000);
+
+    // displaying values
+    dayBox.textContent = addZeroes(daysLeft);
+    hrBox.textContent = addZeroes(hrsLeft);
+    minBox.textContent = addZeroes(minsLeft);
+    secBox.textContent = addZeroes(secsLeft);
+  }
 }
-let  i = setInterval(countdown,1000);
+let i = setInterval(countdown, 1000);
 countdown();
-
-
 
 // for scroll to top
 
@@ -54,16 +53,15 @@ const scrollElement = document.querySelector(".scroll-top");
 
 const scrollMax = document.querySelector(".firstsection");
 
-const scrollTop = ()=>{
-    scrollMax.scrollIntoView({ behaviour: "smooth" });
+const scrollTop = () => {
+  scrollMax.scrollIntoView({ behaviour: "smooth" });
 };
 
 scrollElement.addEventListener("click", scrollTop);
 
-
 // loader
-var preLoader = document.getElementById('loding');
+var preLoader = document.getElementById("loding");
 
-window.addEventListener("load", function(){
-    preLoader.style.display= "none";
-})
+window.addEventListener("load", function () {
+  preLoader.style.display = "none";
+});
