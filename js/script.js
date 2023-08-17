84a8cc3672fe390f531ef4e9ec9c52f4696413cb
RAHUL <dasr16583@gmail.com>
2023-08-17 21:51:18 +0530

    few changes

--- js/script.js
+++ js/script.js
@@ -5,47 +5,44 @@ let minBox = document.getElementById("min-box");
 let secBox = document.getElementById("sec-box");
 
 // year, day, hour, second
-let endDate = new Date(2023, 9, 6, 12);
-let endTime = endDate.getTime();
 
-function countdown() {
-  let todayDate = new Date();
-  let todayTime = todayDate.getTime();
+function updateCountdown(endDate) {
+  const now = new Date().getTime();
+  const remainingTime = endDate - now;
 
-  let reminingTime = endTime - todayTime;
+  const oneMin = 60 * 1000;
+  const oneHr = 60 * oneMin;
+  const oneDay = 24 * oneHr;
 
-  // 60 second = 1000 millisecond
-  let oneMin = 60 * 1000;
-  let oneHr = 60 * oneMin;
-  let oneDay = 24 * oneHr;
+  const addZeroes = (num) => (num < 10 ? `0${num}` : num);
 
-  // function for formate number if it is single digit
-  let addZeroes = (num) => (num < 10 ? `0${num}` : num);
-
-  // if end datee is before today date
-  if (endTime < todayTime) {
+  if (remainingTime <= 0) {
     clearInterval(i);
     document.querySelector(
       ".countdown"
-    ).innerHTML = `<h1>Countdown had expired!</h1>`;
+    ).innerHTML = `<h1>Countdown has expired!</h1>`;
   } else {
-    let daysLeft = Math.floor(reminingTime / oneDay);
+    const daysLeft = Math.floor(remainingTime / oneDay);
+    const hrsLeft = Math.floor((remainingTime % oneDay) / oneHr);
+    const minsLeft = Math.floor((remainingTime % oneHr) / oneMin);
+    const secsLeft = Math.floor((remainingTime % oneMin) / 1000);
 
-    let hrsLeft = Math.floor((reminingTime % oneDay) / oneHr);
-
-    let minsLeft = Math.floor((reminingTime % oneHr) / oneMin);
-
-    let secsLeft = Math.floor((reminingTime % oneMin) / 1000);
-
-    // displaying values
     dayBox.textContent = addZeroes(daysLeft);
     hrBox.textContent = addZeroes(hrsLeft);
     minBox.textContent = addZeroes(minsLeft);
     secBox.textContent = addZeroes(secsLeft);
   }
 }
-let i = setInterval(countdown, 1000);
-countdown();
+
+// Set your desired end date and time
+const endDate = new Date("2023-10-31T23:59:59").getTime();
+
+// Initial call to display the countdown immediately
+updateCountdown(endDate);
+
+// Update the countdown every second (1000 milliseconds)
+let i = setInterval(() => updateCountdown(endDate), 1000);
+
 
 // for scroll to top
 
